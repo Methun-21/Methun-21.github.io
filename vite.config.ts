2e8bee7d46d39f4b7720a9b705c29d7cadf7d02b
methun21 <methunraja7393@gmail.com>
2025-09-27 22:35:09 +0530

    Update repository URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/achievigo-tasks/",
+  base: "/Task-Manager/",
   server: {
     host: "::",
     port: 8080,
