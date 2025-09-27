0a98321c3f9e4cb2a987098065368c398bfdf8ab
methun21 <methunraja7393@gmail.com>
2025-09-27 22:50:21 +0530

    Chore: Remove QueryClientProvider for debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
-const App = () => <h1>Hello, World!</h1>;
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+
+
+const App = () => (
+  <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  </TooltipProvider>
+);
 
 export default App;
